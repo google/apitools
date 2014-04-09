93816681ed7ecf92ee49e12971a74a9d1be6b5e2
Craig Citro <craigcitro@google.com>
2014-04-09 10:10:30 -0700

    2.6 options

--- setup.py
+++ setup.py
@@ -46,7 +46,7 @@ py_version = platform.python_version()
 
 if py_version < '2.7':
   REQUIRED_PACKAGES.append('unittest2==0.5.1')
-#   REQUIRED_PACKAGES.append('argparse==1.2.1')
+  REQUIRED_PACKAGES.append('argparse==1.2.1')
 
 _NAMESPACE = 'apitools'
 _APITOOLS_VERSION = '0.2'
