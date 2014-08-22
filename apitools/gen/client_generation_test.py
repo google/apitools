1a523321ad9d175140e2f3a9d85ddede10632555
Craig Citro <craigcitro@gmail.com>
2014-08-21 17:12:44 -0700

    Add a simple test for generated clients.

---
+++ apitools/gen/client_generation_test.py
@@ -0,0 +1,69 @@
+"""Test gen_client against all the APIs we use regularly."""
+
+import contextlib
+import logging
+import os
+import shutil
+import subprocess
+import tempfile
+
+from google.apputils import basetest as googletest
+
+from apitools.gen import util
+
+_API_LIST = [
+    'drive.v2',
+    'bigquery.v2',
+    'compute.v1',
+    'storage.v1',
+]
+
+@contextlib.contextmanager
+def TempDir():
+  original_dir = os.getcwd()
+  path = tempfile.mkdtemp()
+  try:
+    os.chdir(path)
+    yield path
+  finally:
+    os.chdir(original_dir)
+    shutil.rmtree(path)
+
+
+class ClientGenerationTest(googletest.TestCase):
+
+  def setUp(self):
+    super(ClientGenerationTest, self).setUp()
+    self.gen_client_binary = 'gen_client'
+
+  def testGeneration(self):
+    for api in _API_LIST:
+      with TempDir() as path:
+        args = [
+            self.gen_client_binary,
+            '--client_id=12345',
+            '--client_secret=67890',
+            '--discovery_url=%s' % api,
+            '--outdir=generated',
+            '--overwrite',
+            'client',
+        ]
+        logging.info('Testing API %s with command line: %s', api, ' '.join(args))
+        retcode = subprocess.call(args)
+        if retcode == 128:
+          logging.error('Failed to fetch discovery doc, continuing.')
+          continue
+        self.assertEqual(0, retcode)
+
+        with tempfile.NamedTemporaryFile() as out:
+          cmdline_args = [
+              os.path.join('generated', api.replace('.', '_') + '.py'),
+              'help',
+          ]
+          retcode = subprocess.call(cmdline_args, stdout=out)
+        # appcommands returns 1 on help
+        self.assertEqual(1, retcode)
+
+
+if __name__ == '__main__':
+  googletest.main()
