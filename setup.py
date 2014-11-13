916952a6c79498320ae93532ac101b0eca78e7ef
Craig Citro <craigcitro@google.com>
2014-11-12 16:59:57 -0800

    Make two optional dependencies looser.

--- setup.py
+++ setup.py
@@ -50,8 +50,8 @@ CONSOLE_SCRIPTS = [
 py_version = platform.python_version()
 
 if py_version < '2.7':
-    REQUIRED_PACKAGES.append('unittest2==0.5.1')
-    REQUIRED_PACKAGES.append('argparse==1.2.1')
+    REQUIRED_PACKAGES.append('unittest2>=0.5.1')
+    REQUIRED_PACKAGES.append('argparse>=1.2.1')
 
 _APITOOLS_VERSION = '0.3'
 
