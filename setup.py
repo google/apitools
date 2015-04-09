25aeb184e6b80cc6fa6117d540d0b9ce1d917fa0
Craig Citro <craigcitro@google.com>
2015-04-09 00:26:18 -0700

    Update version for 0.4.2 release.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ py_version = platform.python_version()
 if py_version < '2.7':
     REQUIRED_PACKAGES.append('argparse>=1.2.1')
 
-_APITOOLS_VERSION = '0.4.1'
+_APITOOLS_VERSION = '0.4.2'
 
 with open('README.rst') as fileobj:
     README = fileobj.read()
