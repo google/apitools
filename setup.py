375e1b4541c696fba160bc8c94a61559d61325e7
Craig Citro <craigcitro@google.com>
2015-05-14 13:04:36 -0700

    Update version for v0.4.6 release.

--- setup.py
+++ setup.py
@@ -54,7 +54,7 @@ py_version = platform.python_version()
 if py_version < '2.7':
     REQUIRED_PACKAGES.append('argparse>=1.2.1')
 
-_APITOOLS_VERSION = '0.4.5'
+_APITOOLS_VERSION = '0.4.6'
 
 with open('README.rst') as fileobj:
     README = fileobj.read()
