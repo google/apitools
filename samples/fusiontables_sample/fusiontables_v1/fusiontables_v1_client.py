fd4a7b7fb7a20b729f54f0abb66198e9f34a16de
Mark <markpell@users.noreply.github.com>
2018-03-12 13:08:29 -0400

    Added response_encoding parameter to api client classes. (#206)
    On Python 3, httplib2 returns message contents as bytes, which cannot be
    read by json.loads(). It must first be decoded to text. Specifying this
    parameter tells the client to first decode the body before trying to
    parse it.

--- samples/fusiontables_sample/fusiontables_v1/fusiontables_v1_client.py
+++ samples/fusiontables_sample/fusiontables_v1/fusiontables_v1_client.py
@@ -24,7 +24,7 @@ class FusiontablesV1(base_api.BaseApiClient):
                get_credentials=True, http=None, model=None,
                log_request=False, log_response=False,
                credentials_args=None, default_global_params=None,
-               additional_http_headers=None):
+               additional_http_headers=None, response_encoding=None):
     """Create a new fusiontables handle."""
     url = url or self.BASE_URL
     super(FusiontablesV1, self).__init__(
@@ -33,7 +33,8 @@ class FusiontablesV1(base_api.BaseApiClient):
         log_request=log_request, log_response=log_response,
         credentials_args=credentials_args,
         default_global_params=default_global_params,
-        additional_http_headers=additional_http_headers)
+        additional_http_headers=additional_http_headers,
+        response_encoding=response_encoding)
     self.column = self.ColumnService(self)
     self.query = self.QueryService(self)
     self.style = self.StyleService(self)
