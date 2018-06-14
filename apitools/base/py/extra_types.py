c6f073a26029e203bc062956cdb3fa00b942536f
Harrison Gregg <hgregg@google.com>
2018-06-14 15:31:54 -0400

    Move functionality in encoding.py to encoding_helper.py

--- apitools/base/py/extra_types.py
+++ apitools/base/py/extra_types.py
@@ -26,7 +26,7 @@ import six
 from apitools.base.protorpclite import message_types
 from apitools.base.protorpclite import messages
 from apitools.base.protorpclite import protojson
-from apitools.base.py import encoding
+from apitools.base.py import encoding_helper as encoding
 from apitools.base.py import exceptions
 from apitools.base.py import util
 
