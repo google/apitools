effc2e576427d8c876b1d64c79edcd98ab433074
Matt Houglum <houglum@google.com>
2018-11-15 18:41:19 -0800

    Update for v0.5.25 release. (#248)

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ CONSOLE_SCRIPTS = [
 
 py_version = platform.python_version()
 
-_APITOOLS_VERSION = '0.5.24'
+_APITOOLS_VERSION = '0.5.25'
 
 with open('README.rst') as fileobj:
     README = fileobj.read()
