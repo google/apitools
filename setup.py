48523168d95a90c85291a3366171c21dc775d9ec
Sarah Heacock <sheacock@google.com>
2026-02-23 20:47:20 +0000

    bump apitools version 0.5.36

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ CONSOLE_SCRIPTS = [
 
 py_version = platform.python_version()
 
-_APITOOLS_VERSION = '0.5.35'
+_APITOOLS_VERSION = '0.5.36'
 
 with open('README.rst') as fileobj:
     README = fileobj.read()
